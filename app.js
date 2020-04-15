bec29859c05338c9ff5433f543db10c1a21ba58c
Daniel ArchBoss <danielplata79@gmail.com>
2020-04-15 17:52:17 -0500

    Update 1.2

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ class productos {
 	}
 }
 
+var t = 0;
+
 class userInterface {
 
-	addProduct (producto) {
+	 addProduct(producto) {
+		t++;
 		const listaProductos = document.getElementById ("list-product");
 		const elemento = document.createElement("div");
 		elemento.innerHTML = `
@@ -19,35 +22,42 @@ class userInterface {
 				<div class="item-main-2">
 			
 					<div class="item-main-21">
+						<h5>ID : </h5>
 						<h5>NAME : </h5>
 						<h5>PRICE : </h5>
 						<h5>YEAR : </h5>
 					</div>
 
 					<div class="item-main-22">
+						<h5>${t}</h5>
 						<h5 class="product-price-input">${producto.name}</h5>
-						<h5 class="product-price-input">${producto.price}</h5>
-						<h5 class="product-price-input">${producto.year}</h5>
+						<h5 class="product-price-input">$${producto.price}</h5> <h5 class="product-price-input">${producto.year}</h5>
 					</div>
 
-					<div class="delete-icon">
-
+					<div class='deleteDiv'>
+						<h5>Eliminar</h5>		
 					</div>
+
+					<div class="delete-icon" id='delete-icon' name="${t}">
+					<img src='images/deleteBtn.svg'>
+					</div>
+
+				
 				</div>
 			
 			</div>
 			`;
-
-			const items = document.getElementsByClassName("item").length;
-			const boxOne = document.getElementById("container-big");
-			const  elementoDos = document.createElement("div");
-			elementoDos.innerHTML = `
+	// TOTAL PRODUCTOS
+		const items = document.getElementsByClassName("item").length;
+		const boxOne = document.getElementById("container-big");
+		const  elementoDos = document.createElement("div");
+		elementoDos.innerHTML = `
 			<div id="container-2-alert" name="Eliminar	">
 					<div class="container-2-mainUno">
 						<h4>TOTAL PRODUCTOS :</h4>
 					</div>
 
-					<div class="container-2-mainDos">
+					<div class="container-2-mainDos" id='container-2-mainDos'>
 						<h4 id="itemsFrame">${items + 1}</h4>
 					</div>
 			</div>
@@ -62,8 +72,7 @@ class userInterface {
 	
 	deleteNum(elementoDos) {
 		const boxes = document.getElementsByClassName("item").length;
-		console.log(boxes);
-		if (boxes >= 2 ) {
+			if (boxes >= 2 ) {
 			const r = document.getElementById("container-big");
 			r.removeChild(r.childNodes[1]);	
 		}
@@ -77,40 +86,16 @@ document.getElementById("form-product")
 		const name = document.getElementById("name-product").value;
 		const price = document.getElementById("price-product").value;
 		const year = document.getElementById("year-product").value;
-		console.log("NAME : " + name + " PRICE : " + price + " YEAR : " + year);
+		/*console.log("NAME : " + name + " PRICE : " + price + " YEAR : " + year);*/
 
 		const producto = new productos (name, price, year);
-
 		const interfaz = new  userInterface ();
 
-		// SAVE
+	// SAVE
 		interfaz.addProduct(producto);
 		interfaz.resetForm();
-
-		// SHIT
-		const divs = document.getElementsByClassName("item").length;
-		if (divs === 1) {
-			console.log("Hay " + divs + " Item");
-		} else {
-			console.log("Hay " + divs + " Items");
-		}
-
-
 		interfaz.deleteNum();
-
 		e.preventDefault();
-	} );
+	});
 
 
-document.getElementById("container-big")
-	.addEventListener("click", function(e) {
-		// console.log(e.target);
-		})
-
-$(".item").mouseover(function(){$(".delete-icon").visible()});
-const y = document.getElementsByClassName("item");
-const n = document.getElementsByClassName("delete.icon");
-	y.onmouseover = function () {
-		n.style.visibility="visible"
-
-	}
---
+++ my_jquery.js
@@ -0,0 +1,95 @@
+$(function(){
+
+//DOM ADD ITEM
+var t = 0;
+document.getElementById('form-product')
+	.addEventListener('submit', function (e) {
+
+	$('#empty-list').remove();
+
+	// VAR T
+	const prueba = document.getElementsByClassName('item').length;
+	console.log(prueba);
+	t++;
+	console.log('var t = ' + t);	
+});
+
+var o = null;
+var u = null;
+var t = null;
+// SHOW DELETE BUTTON
+$("#list-product").on('click', '.delete-icon', function() {	
+		clearTimeout(o);
+		clearTimeout(u);
+		clearTimeout(t);	
+	//GLOBAL TIMER OUT
+	t = setTimeout (function () {
+		$('.delete-icon').css('opacity', '0.5');
+		$('.delete-icon').css('background', '#ef5350');
+		$('.deleteDiv').css('margin-top', '110px');
+		$('.deleteDiv').css('visibility', 'hidden');
+		$('.item').css('border-bottom-left-radius', '10px');  	
+		$('.item').css('border-bottom-right-radius', '10px');	
+	}, 7000);
+
+	//SHOW CURRENT DELETE BTN
+	var g = $(this);
+		$(g).css('background-color', '#b71c1c');
+		$(g).css('opacity', '1');
+		$(g).siblings('.deleteDiv').css('margin-top', '140px');
+		$(g).siblings('.deleteDiv').css('visibility', 'visible');
+		$(g).parents('.item').css('border-bottom-left-radius', '0px');
+		$(g).parents('.item').css('border-bottom-right-radius', '0px');	
+	
+	//HIDE 7 S AFTER CLICK TRASH BTN
+	o = setTimeout (function () {
+		$(g).css('background-color', '');
+		$(g).css('opacity', '');
+		$(g).siblings('.deleteDiv').css('border-bottom-left-radius', '10px');
+		$(g).siblings('.deleteDiv').css('border-bottom-right-radius', '10px');
+		$(g).siblings('.deleteDiv').css('visibility', 'hidden');
+		$(g).siblings('.deleteDiv').css('margin-top', '110px');
+		$(g).parents('.item').css('border-bottom-left-radius', '10px');
+		$(g).parents('.item').css('border-bottom-right-radius', '10px');		
+	}, 7000);
+	
+	//IF HOVER WAIT 15 SEGS BEFORE HIDE
+	$("#list-product").on('mouseenter', '.deleteDiv', function() {
+		clearTimeout(t);
+		clearTimeout(o);
+		clearTimeout(u);
+		const s = $(this);
+		u = setTimeout (function () {
+			$(this).siblings('.delete-icon').css('background-color', '');
+			$(this).siblings('.delete-icon').css('opacity', '');
+			$(s).css('border-bottom-left-radius', '10px');
+			$(s).css('border-bottom-right-radius', '10px');
+			$(s).css('visibility', 'hidden');
+			$(s).css('margin-top', '110px');
+			$(s).parents('.item').css('border-bottom-left-radius', '10px');
+			$(s).parents('.item').css('border-bottom-right-radius', '10px');	
+			t = setTimeout (function () {
+				$('.delete-icon').css('opacity', '');
+				$('.delete-icon').css('background', '');
+				$('.deleteDiv').css('margin-top', '110px');
+				$('.deleteDiv').css('visibility', 'hidden');
+				$('.item').css('border-bottom-left-radius', '10px');  	
+				$('.item').css('border-bottom-right-radius', '10px');	
+			}, 3000);
+		}, 15000);
+	});
+});
+
+
+
+//DELETE BUTTON FUNCTION
+$('#list-product').on('click', '.deleteDiv', function() {
+	const prueba2 = document.getElementsByClassName('item').length - 1;
+
+	$(this);
+	$(this).parents('.item').remove();
+	$('#itemsFrame').remove();
+	$('#container-2-mainDos').append("<h4 id='itemsFrame'>" + prueba2 + "</h4>");
+});
+
+}); //end
